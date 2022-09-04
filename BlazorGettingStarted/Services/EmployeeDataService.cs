73493673132ebae7c5e2b83838bf13760ba10920
John Collins <johnscollins98@gmail.com>
2022-09-04 17:16:51 +0100

    add support for adding, editing and deleting employees

--- BlazorGettingStarted/Services/EmployeeDataService.cs
+++ BlazorGettingStarted/Services/EmployeeDataService.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using System.Text.Json;
 using BlazorGettingStarted.Shared;
 
@@ -12,14 +13,24 @@ namespace BlazorGettingStarted.Services
             this.httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
         }
 
-        public Task<Employee> AddEmployee(Employee employee)
+        public async Task<Employee> AddEmployee(Employee employee)
         {
-            throw new NotImplementedException();
+            var employeeJson =
+                new StringContent(JsonSerializer.Serialize(employee), Encoding.UTF8, "application/json");
+
+            var response = await httpClient.PostAsync("api/employee", employeeJson);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await JsonSerializer.DeserializeAsync<Employee>(await response.Content.ReadAsStreamAsync());
+            }
+
+            return null;
         }
 
-        public Task DeleteEmployee(int employeeId)
+        public async Task DeleteEmployee(int employeeId)
         {
-            throw new NotImplementedException();
+            await httpClient.DeleteAsync($"api/employee/{employeeId}");
         }
 
         public async Task<IEnumerable<Employee>> GetAllEmployees()
@@ -34,9 +45,12 @@ namespace BlazorGettingStarted.Services
                 (await httpClient.GetStreamAsync($"api/employee/{employeeId}"), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
         }
 
-        public Task UpdateEmployee(Employee employee)
+        public async Task UpdateEmployee(Employee employee)
         {
-            throw new NotImplementedException();
+            var employeeJson =
+                new StringContent(JsonSerializer.Serialize(employee), Encoding.UTF8, "application/json");
+
+            await httpClient.PutAsync("api/employee", employeeJson);
         }
     }
 }
